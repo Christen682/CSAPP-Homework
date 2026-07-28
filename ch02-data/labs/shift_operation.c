9e3627fd80dc26169e5dc2e0b4f639e47a87951a
KoyukiNya <2940836510@qq.com>
2026-07-28 16:20:21 +0800

    test

--- ch02-data/labs/shift_operation.c
+++ ch02-data/labs/shift_operation.c
@@ -13,6 +13,29 @@ void show_bytes(byte_pointer start, size_t len)
     printf("\n");
 }
 
+int fun1(unsigned word)
+{
+    return (int)((word << 24) >> 24);
+}
+
+int fun2(unsigned word)
+{
+    return ((int)word << 24) >> 24;
+}
+
+/*Warning: This is buggy code */
+float sum_elements(float start[], unsigned length)
+{
+    int i = 0;
+    float result = 0;
+
+    for (i; i < length - 1; i++)
+    {
+        result += start[i];
+    }
+    return result;
+}
+
 int main()
 {
     // short x = 12345;
@@ -20,11 +43,17 @@ int main()
     // show_bytes((byte_pointer)&x, sizeof(x));
     // show_bytes((byte_pointer)&mx, sizeof(-x));
 
-    short sx = -12345;
-    unsigned uy = sx;
+    // short sx = -12345;
+    // unsigned uy = sx;
 
-    printf("uy = %u: \t ", uy);
-    show_bytes((byte_pointer)&uy, sizeof(uy));
+    // printf("uy = %u: \t ", uy);
+    // show_bytes((byte_pointer)&uy, sizeof(uy));
+
+    // printf("fun1 = %x \t ", fun1(0x000000C9));
+    // printf("fun2 = %x \t ", fun2(0x000000C9));
+
+    float arr[5] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    printf("Sum of elements: %f\n", sum_elements(arr, 0));
 
     return 0;
 }
