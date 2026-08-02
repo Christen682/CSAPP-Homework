d811eb5c49abc9861c041064be3809dd69611dbb
Christen <2940836510@qq.com>
2026-08-02 10:58:55 +0800

    1

--- ch02-data/labs/shift_operation.c
+++ ch02-data/labs/shift_operation.c
@@ -44,6 +44,14 @@ int uadd_ok(unsigned x, unsigned y)
     return s >= x;
 }
 
+int tadd_ok(int x, int y)
+{
+    int sum = x + y;
+    int neg_over = x < 0 && y < 0 && sum >= 0;
+    int pos_over = x > 0 && y > 0 && sum < 0;
+    return !neg_over && !pos_over;
+}
+
 int main()
 {
     // short x = 12345;
